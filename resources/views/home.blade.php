1c1c3667e56380c986510df01ee9b3db1508c0ba
Ruth Cedeño <emperatrizcedeno18@gmail.com>
2024-04-04 17:34:37 -0400

    Structure of the categories in Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,102 @@
 @extends('layouts.app')
 
 @section ('content')
-    <section class="card-product">
-        <div class="products">
-            <div class="makeup">
-                <div class="product">
-                    <div class="image">
-                        <p>imagen.png</p>
-                    </div>
-                    <div class="product-detail">
-                        <div class="product-name">
-                            <p>Nombre del Producto</p>
-                        </div>
-                        <div class="product-cost">
-                            <p>Costo del Producto</p>
-                        </div>
-                        <div class="product-buy">
-                            <button>Añadir al carrito</button>
-                        </div>
-                    </div>
-                </div>
+<div class="flex-container">
+    <div class="product">
+        <div class="image">
+            <p>imagen.png</p>
+        </div>
+        <div class="product-detail">
+            <div class="product-name">
+                <p>Nombre del Producto</p>
             </div>
-            <div class="skincare">
-                <div class="product">
-                    <div class="image">
-                        <p>imagen.png</p>
-                    </div>
-                    <div class="product-detail">
-                        <div class="product-name">
-                            <p>Nombre del Producto</p>
-                        </div>
-                        <div class="product-cost">
-                            <p>Costo del Producto</p>
-                        </div>
-                        <div class="product-buy">
-                            <button>Añadir al carrito</button>
-                        </div>
-                    </div>
-                </div>
+            <div class="product-cost">
+                <p>Costo del Producto</p>
             </div>
-            <div class="accesories">
-                <div class="product">
-                    <div class="image">
-                        <p>imagen.png</p>
-                    </div>
-                    <div class="product-detail">
-                        <div class="product-name">
-                            <p>Nombre del Producto</p>
-                        </div>
-                        <div class="product-cost">
-                            <p>Costo del Producto</p>
-                        </div>
-                        <div class="product-buy">
-                            <button>Añadir al carrito</button>
-                        </div>
-                    </div>
-                </div>
+            <div class="product-buy">
+                <button>Añadir al carrito</button>
             </div>
         </div>
-    </section>
+    </div>
+    <div class="product">
+        <div class="image">
+            <p>imagen.png</p>
+        </div>
+        <div class="product-detail">
+            <div class="product-name">
+                <p>Nombre del Producto</p>
+            </div>
+            <div class="product-cost">
+                <p>Costo del Producto</p>
+            </div>
+            <div class="product-buy">
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+    <div class="product">
+        <div class="image">
+            <p>imagen.png</p>
+        </div>
+        <div class="product-detail">
+            <div class="product-name">
+                <p>Nombre del Producto</p>
+            </div>
+            <div class="product-cost">
+                <p>Costo del Producto</p>
+            </div>
+            <div class="product-buy">
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+    <div class="product">
+        <div class="image">
+            <p>imagen.png</p>
+        </div>
+        <div class="product-detail">
+            <div class="product-name">
+                <p>Nombre del Producto</p>
+            </div>
+            <div class="product-cost">
+                <p>Costo del Producto</p>
+            </div>
+            <div class="product-buy">
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+    <div class="product">
+        <div class="image">
+            <p>imagen.png</p>
+        </div>
+        <div class="product-detail">
+            <div class="product-name">
+                <p>Nombre del Producto</p>
+            </div>
+            <div class="product-cost">
+                <p>Costo del Producto</p>
+            </div>
+            <div class="product-buy">
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+    <div class="product">
+        <div class="image">
+            <p>imagen.png</p>
+        </div>
+        <div class="product-detail">
+            <div class="product-name">
+                <p>Nombre del Producto</p>
+            </div>
+            <div class="product-cost">
+                <p>Costo del Producto</p>
+            </div>
+            <div class="product-buy">
+                <button>Añadir al carrito</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
