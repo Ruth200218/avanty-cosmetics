5c2596d205c764e04a7379c8f16334905537cf93
Ruth Cedeño <emperatrizcedeno18@gmail.com>
2024-04-12 16:42:50 -0400

    Part of the nav styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,41 +4,73 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="icon" href="{{ asset('images/icons/logo.png') }}"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Alata&family=Sacramento&display=swap" rel="stylesheet">
     <title>Avanty Cosmetics</title>
 </head>
 <body>
     <header>
+        <div class="custom-shape-divider-top-1712946376">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+            </svg>
+        </div>
         <div class="title-header">
+            <img src="{{ asset('images/moon.png') }}" alt="moon">
             <h1>Avanty Cosmetics</h1>
         </div>
 
-        <nav class="nav-home">
-            <ul>
+        <nav class="menu-container">
+            <input type="checkbox" aria-label="Toggle menu" />
+            <span></span>
+            <span></span>
+            <span></span>
+        
+            <div class="menu">
+              <ul>
                 <li>
+                  <a href="#home">
                     Catalogo
+                  </a>
                 </li>
                 <li>
+                  <a href="#pricing">
                     Maquillaje
+                  </a>
                 </li>
                 <li>
+                  <a href="#blog">
                     Skin Care
+                  </a>
                 </li>
                 <li>
+                  <a href="#docs">
                     Tutoriales
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a href="#signup">
+                    Sign-up
+                  </a>
                 </li>
                 <li>
+                  <a href="#login">
                     Login
+                  </a>
                 </li>
                 <li>
-                    Register
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path><path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path></svg>
+                    </a>
                 </li>
-                <li>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path><path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path></svg>
-                </li>
-            </ul>
-        </nav>
+              </ul>
+            </div>
+          </nav>
 
         <section class="slider-section">
             <div class="carousel">
